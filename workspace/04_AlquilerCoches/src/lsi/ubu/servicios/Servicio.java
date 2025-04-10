26a3c3726fdbe6fb5b2bae34fd341602b2b94537
NicolasVO-dot <nicolasvillanuevaortega@gmail.com>
2025-04-10 17:50:19 +0200

    actualización y reseteo del repo

--- workspace/04_AlquilerCoches/src/lsi/ubu/servicios/Servicio.java
+++
@@ -1,9 +0,0 @@
-package lsi.ubu.servicios;
-
-import java.sql.SQLException;
-import java.util.Date;
-
-public interface Servicio {
-
-	public void alquilar(String nifCliente, String matricula, Date fechaIni, Date fechaFin) throws SQLException;
-}
