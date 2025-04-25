6c0746593a0648a501ac27c990b652fcd70ca483
NicolasVO-dot <nicolasvillanuevaortega@gmail.com>
2025-04-25 12:40:02 +0200

    Gestión de excepciones con enfoque ofensivo

--- workspace/04_AlquilerCoches/src/lsi/ubu/servicios/ServicioImpl.java
+++ workspace/04_AlquilerCoches/src/lsi/ubu/servicios/ServicioImpl.java
@@ -12,6 +12,9 @@ import org.slf4j.LoggerFactory;
 
 import lsi.ubu.excepciones.AlquilerCochesException;
 import lsi.ubu.util.PoolDeConexiones;
+import lsi.ubu.util.exceptions.SGBDError;
+import lsi.ubu.util.exceptions.oracle.OracleSGBDErrorUtil;
+
 
 public class ServicioImpl implements Servicio {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ServicioImpl.class);
@@ -19,11 +22,14 @@ public class ServicioImpl implements Servicio {
 	private static final int DIAS_DE_ALQUILER = 4;
 
 	public void alquilar(String nifCliente, String matricula, Date fechaIni, Date fechaFin) throws SQLException {
+		
 		PoolDeConexiones pool = PoolDeConexiones.getInstance();
 
 		Connection con = null;
+		PreparedStatement selCliente = null;
+		PreparedStatement selVehiculo = null;
+		PreparedStatement selDisponible = null;
 		PreparedStatement insReserva = null;
-		PreparedStatement selReserva = null;
 		ResultSet cursor = null;
 
 		/*
@@ -37,6 +43,8 @@ public class ServicioImpl implements Servicio {
 				throw new AlquilerCochesException(AlquilerCochesException.SIN_DIAS);
 			}
 		}
+		
+		//CREO Q NO SE PUEDE O NO SE HACE ASÍ: CORREGIR
 		else {
 			//Si la fecha de fin es null la calculo sumando a la de inicio los días de alquiler
 			fechaFin = new Date(fechaIni.getTime() + TimeUnit.DAYS.toMillis(DIAS_DE_ALQUILER));
@@ -65,25 +73,51 @@ public class ServicioImpl implements Servicio {
 			 * Recuerda que hay casos donde la fecha fin es nula, por lo que se debe de
 			 * calcular sumando los dias de alquiler (ver variable DIAS_DE_ALQUILER) a la
 			 * fecha ini.
+			 * 
 			 */
 			
 			//Inicializo la conexión a la base de datos
 			con = pool.getConnection();
 			
-			// Defino strings con las consultas para hacer el proceso mas ordenado
-			String sqlCliente = "SELECT NIF FROM clientes WHERE NIF = ?";
-			String sqlCoche = "SELECT matricula FROM vehiculos WHERE matricula = ?";
+			/*-----------------------------------------------------------------------------------------*/
 			
-			//Realizo la primera comprobacion para saber si existe el cliente
-			selReserva = con.prepareStatement(sqlCliente);
-			selReserva.setString(1,nifCliente);
+			//Realizo la comprobacion para saber si existe el vehículo
+			selVehiculo = con.prepareStatement("SELECT matricula FROM vehiculos WHERE matricula = ?");
+			selVehiculo.setString(1,matricula);
 			
-			cursor = selReserva.executeQuery();
+			cursor = selVehiculo.executeQuery();
 			if (!cursor.next()) {
-				throw new AlquilerCochesException(AlquilerCochesException.CLIENTE_NO_EXIST);
+				throw new AlquilerCochesException(AlquilerCochesException.VEHICULO_NO_EXIST);
+			}
+			cursor.close();
+			
+			//Si cuando insertemos el cliente este no existe, se violará la fk, se capturará en el catch y se propaga la correcta
+			
+			/*-----------------------------------------------------------------------------------------*/
+			
+			//Ahora comprobaré la disponibilidad del coche introducido en las fechas especificadas
+			selDisponible = con.prepareStatement("SELECT fecha_ini, fecha_fin FROM reservas WHERE matricula = ?");
+			selDisponible.setString(1,matricula);
+			
+			cursor = selDisponible.executeQuery();
+			while(cursor.next()) {
+				
 			}
 			
-			//La existencia de coche la puedo comprobar igual o luego capturando FK_VIOLATED
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 			
@@ -104,6 +138,13 @@ public class ServicioImpl implements Servicio {
 			if(e instanceof AlquilerCochesException) {
 				throw (AlquilerCochesException) e;
 			}
+			
+			if(new OracleSGBDErrorUtil().checkExceptionToCode(e, SGBDError.FK_VIOLATED)) {
+				//En caso de que se viole una clave foránea, compruebo cual es y saco la excepción adecuada
+				//Pero si no se viola la clave no necesito comprobar nada y me lo ahorro
+				//Sin embargo si compruebo fuera la de clientes, la uníca Fk q podríamos violar es la de coches
+				throw new AlquilerCochesException(AlquilerCochesException.CLIENTE_NO_EXIST);
+			}
 
 			// Esto es lo q estaba por defecto en el archivo
 			LOGGER.debug(e.getMessage());
@@ -116,8 +157,12 @@ public class ServicioImpl implements Servicio {
 				insReserva.close();
 			}
 			
-			if (selReserva != null) {
-				selReserva.close();
+			if (selCliente != null) {
+				selCliente.close();
+			}
+			
+			if (selVehiculo != null) {
+				selVehiculo.close();
 			}
 			
 			if (cursor != null) {
